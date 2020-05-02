0621b720ef746640c4ac86e4efe0e57e74085841
yongsookim <yongsookim.com@gmail.com>
2020-05-02 21:24:08 +0900

    second commit

---
+++ src/test/java/com/cidemo/cidemo/CidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidemo.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
